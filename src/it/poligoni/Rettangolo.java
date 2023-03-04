d4b91b4bc408555116fc16f4657016370256441b
MyPC <MyPC@DESKTOP-CITU7RE>
2023-03-04 09:12:54 +0100

    Creazione della classe Rettangolo 04/03/2023 v.1.0

---
+++ src/it/poligoni/Rettangolo.java
@@ -0,0 +1,33 @@
+package it.poligoni;
+
+public class Rettangolo {
+	private int base;
+	private int altezza;
+	
+	public Rettangolo() {	
+	}
+
+	public int getBase() {
+		return base;
+	}
+
+	public void setBase(int base) {
+		this.base = base;
+	}
+
+	public int getAltezza() {
+		return altezza;
+	}
+
+	public void setAltezza(int altezza) {
+		this.altezza = altezza;
+	}
+
+	@Override
+	public String toString() {
+		return "Rettangolo [base=" + base + ", altezza=" + altezza + "]";
+	}
+	
+	
+
+}
